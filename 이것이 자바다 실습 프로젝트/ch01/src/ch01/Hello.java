85a60d979f3f87455fd4c6ef9cafa08b2449ce2b
gunhyunkim <zilu11@naver.com>
2018-04-13 17:28:33 +0900

    println 추가

--- 이것이 자바다 실습 프로젝트/ch01/src/ch01/Hello.java
+++ 이것이 자바다 실습 프로젝트/ch01/src/ch01/Hello.java
@@ -3,7 +3,7 @@ package ch01;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello, welcome to the java world!!");
 
 	}
 
