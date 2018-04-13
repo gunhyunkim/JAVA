bced440e083a93f491489fd6738ec9a9c473a5b7
gunhyunkim <zilu11@naver.com>
2018-04-13 17:45:30 +0900

    예제에 주석 사용

--- 이것이 자바다 실습 프로젝트/ch01/src/ch01/Hello.java
+++ 이것이 자바다 실습 프로젝트/ch01/src/ch01/Hello.java
@@ -1,10 +1,16 @@
 package ch01;
 
-public class Hello {
+/*
+ 작성자 : 김건현
+ 작성일 : 2018.04.15
+ */
 
+public class Hello {
+	//프로그램 실행 진입점
 	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
 		System.out.println("hello, welcome to the java world!!");
 
-	}
+	}	//end of main()
 
 }
